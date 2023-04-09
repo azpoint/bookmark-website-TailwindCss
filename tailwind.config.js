59a07f79bb948827a55588eb47b0d16d3e4b9a28
Alejandro Zapata <alejo8708@gmail.com>
2023-04-09 19:13:19 +0200

    site-end

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/index.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ["./dist/index.{html,js}"],
+    theme: {
+        screens: {
+            sm: "480px",
+            md: "768px",
+            lg: "1020px",
+            xl: "1440px",
+        },
+        extend: {
+            colors: {
+                softBlue: "hsl(231, 69%, 60%)",
+                softRed: "hsl(0, 94%, 66%)",
+                grayishBlue: "hsl(229, 8%, 60%)",
+                veryDarkBlue: "hsl(229, 31%, 21%)",
+            },
+            fontFamily: {
+                sans: ["Rubik", "sans-serif"],
+            },
+            backgroundImage: () => ({
+                dots: "url('../dist/images/bg-dots.svg')",
+            }),
+        },
+    },
+    plugins: [],
+};
